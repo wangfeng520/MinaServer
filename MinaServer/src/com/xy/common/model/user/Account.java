7ecfde55e2ebbd0e85b0757a5e0b72d5ee8bdaf0
wangfeng <wangfengyanrong@gmail.com>
2014-06-25 21:52:26 +0800

    调整

--- MinaServer/src/com/xy/common/model/user/Account.java
+++ MinaServer/src/com/xy/common/model/user/Account.java
@@ -1,60 +1,52 @@
 package com.xy.common.model.user;
 
+import java.util.Date;
+
 import com.xy.common.struct.IdBean;
 
 public class Account extends IdBean {
 
 	private static final long serialVersionUID = 1L;
 	private long id;
-	private String name;
+	private long userId;
 	private String login;
 	private String psw;
-	private String qq;
-	
-
-	
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-
-
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	public String getPsw() {
-		return psw;
-	}
-
-	public void setPsw(String psw) {
-		this.psw = psw;
-	}
-
-	public String getQq() {
-		return qq;
-	}
-
-	public void setQq(String qq) {
-		this.qq = qq;
-	}
-
+	private int count;
+	private Date lastDate;
 	public long getId() {
 		return id;
 	}
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
-
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPsw() {
+		return psw;
+	}
+	public void setPsw(String psw) {
+		this.psw = psw;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public Date getLastDate() {
+		return lastDate;
+	}
+	public void setLastDate(Date lastDate) {
+		this.lastDate = lastDate;
+	}
 }
--- MinaServer/src/com/xy/common/model/user/User.java
+++ MinaServer/src/com/xy/common/model/user/User.java
@@ -6,55 +6,60 @@ public class User extends IdBean {
 
 	private static final long serialVersionUID = 1L;
 	private long id;
-	private String name;
-	private String login;
-	private String psw;
-	private String qq;
+	private String name;  // 玩家昵称
+	private int level;	  // 当前等级
+	private int exp;	  // 当前经验
+ 	private int maxexp;   // 当前等级最大经验值
+	private int gold;     // 金币
+	private int silver;	  // 银币
+	private int power;    // 体力
 	
-
-	
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-
-
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	public String getPsw() {
-		return psw;
-	}
-
-	public void setPsw(String psw) {
-		this.psw = psw;
-	}
-
-	public String getQq() {
-		return qq;
-	}
-
-	public void setQq(String qq) {
-		this.qq = qq;
-	}
-
 	public long getId() {
 		return id;
 	}
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
-
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public int getExp() {
+		return exp;
+	}
+	public void setExp(int exp) {
+		this.exp = exp;
+	}
+	public int getMaxexp() {
+		return maxexp;
+	}
+	public void setMaxexp(int maxexp) {
+		this.maxexp = maxexp;
+	}
+	public int getGold() {
+		return gold;
+	}
+	public void setGold(int gold) {
+		this.gold = gold;
+	}
+	public int getSilver() {
+		return silver;
+	}
+	public void setSilver(int silver) {
+		this.silver = silver;
+	}
+	public int getPower() {
+		return power;
+	}
+	public void setPower(int power) {
+		this.power = power;
+	}
 }
