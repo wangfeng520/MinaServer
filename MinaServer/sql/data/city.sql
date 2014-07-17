18c44c861276574e64b625da988f6652869af5a3
wangfeng <wangfengyanrong@gmail.com>
2014-07-17 23:24:30 +0800

    city

---
+++ MinaServer/sql/data/city.sql
@@ -0,0 +1,5 @@
+copy t_fuben(id, name, icon, type, level, quality, des) from stdin;
+1	'副本1'	'newResources/fuben/fb1.png'	1	1	1	'副本描述测试'	
+2	'副本2'	'newResources/fuben/fb2.png'	1	1	1	'副本描述测试2'	
+3	'副本3'	'newResources/fuben/fb3.png'	1	1	1	'副本描述测试3'	
+\.
--- MinaServer/sql/schema/city.sql
+++ MinaServer/sql/schema/city.sql
@@ -19,7 +19,7 @@ create table t_fuben
 (
 	id						bigserial,
 	name					varchar(64)        not null default '',				-- 副本名称
-	icon					varchar(32)		   not null,						-- 关卡图标	
+	icon					varchar(64)		   not null,						-- 关卡图标	
 	type					int4			   not null default 1,      		-- 类型 1：普通副本 2：精英副本 3:活动副本
 	level       			int4,												-- 最低等级要求 
 	quality					int4               not null default 1,				-- 品质难度低到高1，2，3
