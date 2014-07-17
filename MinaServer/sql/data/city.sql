1a24b9c961d69a34d58dba14ee8e53b56c7d2ec7
wangfeng <wangfengyanrong@gmail.com>
2014-07-17 23:29:41 +0800

    1

--- MinaServer/sql/data/city.sql
+++ MinaServer/sql/data/city.sql
@@ -1,5 +1,5 @@
 copy t_fuben(id, name, icon, type, level, quality, des) from stdin;
-1	'副本1'	'newResources/fuben/fb1.png'	1	1	1	'副本描述1'	
+1	'副本1'	'newResources/fuben/fb1.png'	1	1	1	'副本描述1'
 2	'副本2'	'newResources/fuben/fb2.png'	1	1	1	'副本test2'	
 3	'副本3'	'newResources/fuben/fb3.png'	1	1	1	'副本test3'	
 \.
